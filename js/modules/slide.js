947f31862fa643accd1ea5c3882715509d56144a
Nicolas Bueno <nickibueno@gmail.com>
2022-05-10 22:12:00 -0300

    continue slide

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -2,26 +2,46 @@ export default function slide() {
     const button = document.querySelector(".next");
     const buttonPrev = document.querySelector(".prev");
     const itens = document.querySelector(".slide-itens");
-    let cont = 1
+    let cont = 0
 
     function slide() {
-
-        /*     itens.forEach((item) => {
-                console.log(item.offsetLeft)
-            }) */
-
-        if (cont < 5) {
+   
+        if (cont < itens.children.length -1) {
             cont++;
             itens.style.transform = `translate3d(-${cont*300}px, 0px, 0px)`;
         }
 
+        else{
+        itens.style.transform = `translate3d(-0px, 0px, 0px)`;
+          cont = 0
+
+        }
     }
 
 
+    function slidePrev() {
+   
+        if (cont <= itens.children.length -1) {
+            cont--;
+            itens.style.transform = `translate3d(-${cont*300}px, 0px, 0px)`;
+
+        }
+            else if(cont = -1){   cont= 0
+ }
+             
+
+    
+    }
+
+
+
+
     button.addEventListener("click", slide)
+    buttonPrev.addEventListener("click", slidePrev)
 
 
 
 
 
-}
+
+}   
