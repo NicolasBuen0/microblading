6284877f331fe1baf6cadcf02fd8761226d50c97
Nicolas Bueno <nickibueno@gmail.com>
2022-05-18 15:04:38 -0300

    servicos update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 /* Import Menu */
 import menuHamburguer from './modules/menuHamburguer.js';
-menuHamburguer()
+menuHamburguer();
 
 /* Import Scroll suave */
 import scrollSuave from './modules/scrollSuave.js';
-scrollSuave()
-
-
-/* Import Slide */
-import slide from './modules/slide.js';
-slide()
+scrollSuave();
